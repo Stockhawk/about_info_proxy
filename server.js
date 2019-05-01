b32176995498d0ccc04a057c9c893120961dee70
Ubuntu <ubuntu@ip-172-31-11-115.us-west-1.compute.internal>
2019-05-01 19:35:38 +0000

    Rewrite routes to match new module paths

--- server.js
+++ server.js
@@ -4,57 +4,79 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const URLS = require('./URLS.js');
+console.log(URLS);
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/stocks/:ticker', express.static(path.join(__dirname, 'public')));
 
-const axios3001 = axios.create({
-  baseURL: 'http://localhost:3001',
+const charts = axios.create({ baseURL: URLS.charts, });
+const aboutInfo = axios.create({ baseURL: URLS.aboutInfo, });
+const buySell = axios.create({ baseURL: URLS.buySell, });
+const ratingsHistory = axios.create({ baseURL: URLS.ratingsHistory, });
+
+//
+// CHARTS
+//
+
+app.use('/api/stocks/:symbol/prices', (req, res) => {
+  charts.get(`/api/stocks/${req.params.symbol}/prices`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(500).send(err));
 });
 
-const axios3002 = axios.create({
-  baseURL: 'http://localhost:3002',
-});
-
-const axios3003 = axios.create({
-  baseURL: 'http://localhost:3003',
-});
-
-app.use('/api/ratings/:ticker', (req, res) => {
-  axios3001.get(`/api/ratings/${req.params.ticker}`)
-    .then(response => res.send(response.data))
-    .catch(err => res.send(err));
-})
-
-app.use('/api/history/:ticker', (req, res) => {
-  axios3001.get(`/api/history/${req.params.ticker}`)
-    .then(response => res.send(response.data))
-    .catch(err => res.send(err));
-})
-
-app.use('/api/stocks/:ticker', (req, res) => {
-  axios3002.get(`/api/stocks/${req.params.ticker}`)
-    .then(response => res.send(response.data))
-    .catch(err => res.send(err));
-})
-
-app.use('/api/accounts/:account_number', (req, res) => {
-  axios3002.get(`/api/accounts/${req.params.account_number}`)
-    .then(response => res.send(response.data))
-    .catch(err => res.send(err));
-})
+//
+// ABOUT / INFO
+//
 
 app.use('/api/quotes/:symbol', (req, res) => {
-  axios3003.get(`/api/quotes/${req.params.symbol}`)
+  aboutInfo.get(`/api/stocks/${req.params.symbol}`)
     .then(response => res.send(response.data))
-    .catch(err => res.send(err));
-})
+    .catch(err => res.status(500).send(err));
+});
 
-app.use('/stocks/tags/:tag', (req, res) => {
-  axios3003.get(`/stocks/tags/${req.params.tag}`)
+app.use('/api/tags/:symbol', (req, res) => {
+  aboutInfo.get(`/api/stocks/${req.params.symbol}`)
     .then(response => res.send(response.data))
-    .catch(err => res.send(err));
-})
+    .catch(err => res.status(500).send(err));
+});
+
+//
+// BUY / SELL
+//
+
+app.use('/api/stocks/:symbol', (req, res) => {
+  buySell.get(`/api/stocks/${req.params.symbol}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(500).send(err));
+});
+
+app.use('/api/accounts/:account_number', (req, res) => {
+  buySell.get(`/api/accounts/${req.params.account_number}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(500).send(err));
+});
+
+//
+// RATINGS
+//
+
+app.use('/api/stocks/:symbol/ratings', (req, res) => {
+  charts.get(`/api/stocks/${req.params.symbol}/ratings`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(500).send(err));
+});
+
+app.use('/api/stocks/:symbol/history', (req, res) => {
+  charts.get(`/api/stocks/${req.params.ticker}/history`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(500).send(err));
+});
+
+//
+// PROXY SERVER LISTEN
+//
 
 app.listen(port, () => {
-  console.log(`proxy server running at: http://localhost:${port}`);
+  console.log(`[Server] Listening on port ${port}`);
 });
